e72f303c731de71e01aab9cc0fa220ca294703b7
guyunhome <297890152@qq.com>
2014-12-29 10:33:46 +0800

    ss

--- src/com/sss/MMM.java
+++ src/com/sss/MMM.java
@@ -1,5 +1,11 @@
 package com.sss;
 
-public class MMM {
+import android.app.Activity;
+import android.os.Bundle;
 
+public class MMM extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
 }
